80edc3e286d4a1abb6d140fc7363044b5f00d4c6
Ronan <ronan.bonnet42@wanadoo.fr>
2023-10-09 15:18:46 +0300

    Init

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.conduiteprojet {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.conduiteprojet to javafx.fxml;
-    exports com.example.conduiteprojet;
-}
