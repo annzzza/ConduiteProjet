444818d2ae8c3db2aebf934e4b844805c88d5fff
annzzza <annacazeneuve@gmail.com>
2023-11-21 14:10:43 +0100

    browser loader implement

---
+++ src/main/java/com/example/conduiteprojet/BrowserLoader.java
@@ -0,0 +1,19 @@
+package com.example.conduiteprojet;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class BrowserLoader extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("browser-view.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Browse through Assistances");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
