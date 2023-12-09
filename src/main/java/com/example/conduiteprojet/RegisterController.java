1d8d263b2c96d116b870bd069ecfb18f0b40c594
Ronan <ronan.bonnet42@wanadoo.fr>
2023-12-09 14:57:34 +0100

    Récupérer fenetre principal et avancement de Anna.

--- src/main/java/com/example/conduiteprojet/RegisterController.java
+++ src/main/java/com/example/conduiteprojet/RegisterController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
