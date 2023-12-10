94916e5a3d2a3a64043b27e0ac63d652ce0eb2a2
Ronan <ronan.bonnet42@wanadoo.fr>
2023-12-10 13:06:56 +0100

    Add workflow to create package and upload it on GitHub.

---
+++ src/main/java/com/example/conduiteprojet/Main.java
@@ -0,0 +1,9 @@
+package com.example.conduiteprojet;
+
+import com.example.conduiteprojet.auth.LogInLoader;
+
+public class Main {
+    public static void main(String[] args) {
+        LogInLoader.main(args);
+    }
+}
